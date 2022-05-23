56fa4f47d416cd2bfa5c193b6d5afc8c185d44ba
DesuDG <desuge38@gmail.com>
2022-05-23 23:12:47 +0300

    source code

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>DG Tech</title>
+</head>
+<body>
+  <div class="main">
+    <div class="navebar">
+      <div class="icon">
+        <h2 class="logo">DG Tech </h2>
+      </div>
+      <div class="menu">
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">ABOUT</a></li>
+          <li><a href="#">SERVICE</a></li>
+          <li ><a href="#"  >DESIGN</a></li>
+          <li><a href="#">CONTACT</a></li>
+          <li ><a href="#"  >Login</a></li>
+          <li><a href="#">Signup</a></li>
+
+        </ul>
+      </div>
+    </div>
+
+    <div class="cont">
+      <h1>Web Design & <br> <span>Development</span> <br> Course</h1>
+      <p class="para"> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. <br> Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. </p>
+   
+
+      <button class="learn"><a href="#">Start Now</a></button>
+    </div>
+  </div>
+</body>
+</html>
